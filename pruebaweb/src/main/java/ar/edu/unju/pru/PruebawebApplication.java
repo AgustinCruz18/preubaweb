f2caf417d927f2d8a296f019df1d51cfe989c70a
CRUZ <CRUZ@AGUSTIN>
2023-05-12 03:16:29 +0200

    Proyecto web

---
+++ pruebaweb/src/main/java/ar/edu/unju/pru/PruebawebApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.pru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebawebApplication.class, args);
+	}
+
+}
